8858a65420c6b5aa7f36707582a6485310fef251
ctminh <minh.thanh.chung@ifi.lmu.de>
2024-01-23 16:40:29 +0100

    updated proact-offload algs

--- src/proact_offload1_lrb.py
+++ src/proact_offload1_lrb.py
@@ -35,10 +35,10 @@ def statistics_summary(arr_tasks, num_procs, num_tasks_per_proc):
     avg_load = np.average(load_arr)
     print('-------------------------------------------')
     print('Total load per process: {}'.format(load_arr))
-    print('   + Max: {}'.format(max_load))
-    print('   + Min: {}'.format(min_load))
-    print('   + Avg: {}'.format(avg_load))
-    print('   + Imb: {}'.format(max_load/avg_load - 1))
+    print('   + Max: {:.3f}'.format(max_load))
+    print('   + Min: {:.3f}'.format(min_load))
+    print('   + Avg: {:.3f}'.format(avg_load))
+    print('   + Imb: {:.3f}'.format(max_load/avg_load - 1))
     print('-------------------------------------------\n')
     
     return load_arr
@@ -51,14 +51,27 @@ def proact_task_rebalancing(arr_local_load, arr_remote_load, arr_num_local_tasks
     # check total load info and sort proc ids
     arr_total_load = []
     for i in range(NUM_PROCS):
-        arr_total_load.append(arr_local_load[i]+arr_remote_load[i])
-    Lmax = np.max(arr_total_load)
+        total_load_val = arr_local_load[i]+arr_remote_load[i]
+        arr_total_load.append(total_load_val)
     Lavg = np.average(arr_total_load)
 
     sorted_proc_ids = np.argsort(arr_total_load)
+
+    # extract overloaded and underloaded process ids
+    NUM_OVERLOAD_PROCS = []
+    NUM_UNDERLOAD_PROCS = []
+    for i in sorted_proc_ids:
+        L = arr_total_load[i]
+        if L < Lavg:
+            NUM_UNDERLOAD_PROCS.append(i)
+        elif L > Lavg:
+            NUM_OVERLOAD_PROCS.append(i)
+
     print('-------------------------------------------')
     print('Total load array: {}'.format(arr_local_load))
     print('Sorted process indices: {}'.format(sorted_proc_ids))
+    print('  + Overloaded processes: {}'.format(NUM_OVERLOAD_PROCS))
+    print('  + Underloaded processes: {}'.format(NUM_UNDERLOAD_PROCS))
     print('-------------------------------------------\n')
 
     # create a tracking table for local and remote tasks
@@ -79,10 +92,10 @@ def proact_task_rebalancing(arr_local_load, arr_remote_load, arr_num_local_tasks
     print('-------------------------------------------')
     print('Rebalancing the load: ')
 
-    for i in range(NUM_PROCS):
+    for i in NUM_UNDERLOAD_PROCS:
 
         # get the most underloaded process | left to right
-        victim = sorted_proc_ids[i]
+        victim = i
         victim_load = arr_total_load[victim]
 
         # if the victim load < average
@@ -95,13 +108,13 @@ def proact_task_rebalancing(arr_local_load, arr_remote_load, arr_num_local_tasks
             print('-------------------------------------------')
 
             # checking the most overloaded process
-            for j in range(NUM_PROCS-1, 0, -1):
+            for j in range(len(NUM_OVERLOAD_PROCS)-1, 0, -1):
 
-                offloader = sorted_proc_ids[j]
+                offloader = NUM_OVERLOAD_PROCS[j]
                 offloader_load = arr_total_load[offloader]
                 overloaded_val = offloader_load - Lavg
 
-                print("---> Checking the overloaded P{}: overloaded_val={:.3f}".format(j, overloaded_val))
+                # print("---> Checking the overloaded P{}: overloaded_val={:.3f}".format(offloader, overloaded_val))
 
                 if offloader_load > Lavg and overloaded_val >= LOAD_PER_TASK_PER_PROC[offloader]/2:
                     print('      + Process P{} is overloaded'.format(offloader))
@@ -116,30 +129,30 @@ def proact_task_rebalancing(arr_local_load, arr_remote_load, arr_num_local_tasks
                         numtasks_can_migrate = round(overloaded_val / LOAD_PER_TASK_PER_PROC[offloader])
                         migrated_load = numtasks_can_migrate * LOAD_PER_TASK_PER_PROC[offloader]
                     
-                    print('      + Process P{} migrate {} task(s) to P{}, migrated_load={}'.format(offloader,
+                    print('      + Process P{} migrate {} task(s) to P{}, migrated_load={:.3f}'.format(offloader,
                                                                 numtasks_can_migrate, victim, migrated_load))
                     
                     # update underloaded value, and local load of offloader, remote load of victim
                     underloaded_val = underloaded_val - migrated_load
                     arr_local_load[offloader] -= migrated_load
                     arr_remote_load[victim] += migrated_load
-                    print('      + New underloaded value at P{}: {}'.format(victim, underloaded_val))
+                    print('      + New underloaded value at P{}: {:.3f}'.format(victim, underloaded_val))
                     
                     # update the number of tasks inc. local tasks for offloader, remote tasks for victim
                     arr_num_local_tasks[offloader] -= numtasks_can_migrate
                     arr_num_remote_tasks[victim] += numtasks_can_migrate
                     print('      -----------------------')
-                    print('      + Updated local load at victim P{}: {}'.format(victim, arr_local_load[victim]))
-                    print('      + Updated remote load at victim P{}: {}'.format(victim, arr_remote_load[victim]))
-                    print('      + Updated local load at offloader P{}: {}'.format(offloader, arr_local_load[offloader]))
-                    print('      + Updated remote load at offloader P{}: {}'.format(offloader, arr_remote_load[offloader]))
+                    print('      + Updated local  load at victim P{}: {:.3f}'.format(victim, arr_local_load[victim]))
+                    print('      + Updated remote load at victim P{}: {:.3f}'.format(victim, arr_remote_load[victim]))
+                    print('      + Updated local  load at offloader P{}: {:.3f}'.format(offloader, arr_local_load[offloader]))
+                    print('      + Updated remote load at offloader P{}: {:.3f}'.format(offloader, arr_remote_load[offloader]))
                     print('      -----------------------')
 
                     # update the total load value of both offloader and victim
                     arr_total_load[offloader] = arr_local_load[offloader] + arr_remote_load[offloader]
                     arr_total_load[victim] = arr_local_load[victim] + arr_remote_load[victim]
-                    print('      + Updated total load at victim P{}: {}'.format(victim, arr_total_load[victim]))
-                    print('      + Updated total load at offloader P{}: {}'.format(offloader, arr_total_load[offloader]))
+                    print('      + Updated total load at victim    P{}: {:.3f}'.format(victim, arr_total_load[victim]))
+                    print('      + Updated total load at offloader P{}: {:.3f}'.format(offloader, arr_total_load[offloader]))
                     print('      -----------------------')
 
                     # update the tracking table
@@ -150,7 +163,7 @@ def proact_task_rebalancing(arr_local_load, arr_remote_load, arr_num_local_tasks
 
                     # stop condition
                     abs_value = abs(arr_total_load[victim] - Lavg)
-                    print('      + Abs(victim load P{} - Lavg) = {}'.format(victim, abs_value))
+                    print('      + Abs(victim load P{} - Lavg) = {:.3f}\n'.format(victim, abs_value))
 
                     if abs_value < LOAD_PER_TASK_PER_PROC[offloader]:
                         break
@@ -196,3 +209,4 @@ if __name__ == "__main__":
             if  i != j and table_task_migration[i][j] > 0:
                 print('  + P{}: migrates {} tasks to P{}'.format(i, table_task_migration[i][j], j))
     print('-------------------------------------------\n')
+
